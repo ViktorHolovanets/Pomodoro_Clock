b8ed53744934923dcede7d71555f30e660914ee8
prostou32 <56264273+prostou32@users.noreply.github.com>
2022-06-20 21:37:03 +0300

    Register Pomodoro

--- Pomodoro_Clock/Pomodoro_Register/Program.cs
+++ Pomodoro_Clock/Pomodoro_Register/Program.cs
@@ -16,6 +16,7 @@ namespace Pomodoro_Register
             
 
             Console.ReadKey();
+            Console.Clear();
         }
     }
 }
--- Pomodoro_Clock/Pomodoro_Register/RegSubject/RegPomodoro.cs
+++ Pomodoro_Clock/Pomodoro_Register/RegSubject/RegPomodoro.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Win32;
+using Pomodoro_Register.Rainbow;
 
 namespace Pomodoro_Register.RegSubject
 {
     public class RegPomodoro
     {
+        RegistryKey key;
+
+        public RegPomodoro()
+        {
+            key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE");
+        }
+
         public void Start()
         {
             CreateSubject();
@@ -16,20 +24,26 @@ namespace Pomodoro_Register.RegSubject
 
         private void CreateSubject()
         {
-            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE");
 
-            if (key != null)
-            {
-                
-                Console.WriteLine("Yes");
-            }else
-            {
-
-            }
             
-            foreach (var item in key.GetSubKeyNames())
-            {
-                Console.WriteLine(item);
+           if (key != null)
+           {
+                key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Pomodoro_Clock");
+                if (key == null)
+                {
+                    key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Pomodoro_Clock");
+                    rainbow.TextPaint(ConsoleColor.DarkRed, key + " ", 1);
+
+                }
+           }else
+           {
+                key = Registry.CurrentUser.OpenSubKey(@"Software\Pomodoro_Clock");
+                if (key == null)
+                {
+                    key = Registry.CurrentUser.CreateSubKey(@"Software\Pomodoro_Clock");
+                    rainbow.TextPaint(ConsoleColor.DarkRed, key + " ", 1);
+
+                }
             }
 
             key.Close();
