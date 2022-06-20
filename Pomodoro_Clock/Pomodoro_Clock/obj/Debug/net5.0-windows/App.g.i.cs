a597021d4c32cc91e7b72490204b2b0fa47ba1d3
prostou32 <56264273+prostou32@users.noreply.github.com>
2022-06-20 21:44:13 +0300

    Reg

--- Pomodoro_Clock/Pomodoro_Clock/obj/Debug/net5.0-windows/App.g.i.cs
+++ Pomodoro_Clock/Pomodoro_Clock/obj/Debug/net5.0-windows/App.g.i.cs
@@ -48,7 +48,7 @@ namespace Pomodoro_Clock {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -60,7 +60,7 @@ namespace Pomodoro_Clock {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Pomodoro_Clock;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Pomodoro_Clock;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace Pomodoro_Clock {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
         public static void Main() {
             Pomodoro_Clock.App app = new Pomodoro_Clock.App();
             app.InitializeComponent();
