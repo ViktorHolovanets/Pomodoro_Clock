a74b44bc920b4ddf026472d279e3a8c4bea6c9c6
prostou32 <56264273+prostou32@users.noreply.github.com>
2022-06-26 16:50:57 +0300

    1

--- Pomodoro_Clock/PomodoroRegister/ProgramScriptFille.cs
+++ Pomodoro_Clock/PomodoroRegister/ProgramScriptFille.cs
@@ -1,29 +1,22 @@
 ﻿using System;
-using PomodoroRegister.SubSubject;
-using PomodoroRegister.SubSubject.Path;
 
 namespace PomodoroRegister
 {
     public class ProgramScriptFille
     {
         RegSubject reg = new RegSubject();
-        pathSubject path = new pathSubject();
 
         public void Start()
         {
             reg.Start();
-            path.Start();
         }
         public void Update()
         {
-           // path.AddSubjectSett("Parametr 1", 1);
-           // path.GetSubjectSett("Parametr 1");
             
             
         }
         public void Exit()
         {
-            path.Exit();
             reg.Exit();
         }
     }
